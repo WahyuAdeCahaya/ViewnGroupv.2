dc7fd4afeb6acb948f2416c1f595e3bf86ee3311
delxcign <57690820+delxcign@users.noreply.github.com>
2020-08-29 10:50:36 +0700

    1st

---
+++ app/src/main/java/id/sch/smktelkom/viewngroupv2/MainActivity.java
@@ -0,0 +1,19 @@
+package id.sch.smktelkom.viewngroupv2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Google Pixel");
+        }
+
+    }
+}
